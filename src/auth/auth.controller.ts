9f9ff1998da3ae4d8568f121c56b1d50470bb6b2
Mauricio Mutte <mauriciomutte.mm@gmail.com>
2023-08-22 22:01:15 -0300

    feat(auth): add dto in controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,17 +1,19 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+
 import { AuthService } from './auth.service';
+import { CreateUserDto, LoginDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('signup')
-  async signUp() {
-    return this.authService.signUp();
+  async signUp(@Body() createUserDto: CreateUserDto) {
+    return this.authService.signUp(createUserDto);
   }
 
   @Post('signin')
-  async signIn() {
-    return this.authService.signIn();
+  async signIn(@Body() loginDto: LoginDto) {
+    return this.authService.signIn(loginDto);
   }
 }
