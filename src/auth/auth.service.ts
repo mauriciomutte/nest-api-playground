0647b330c053061a9f9d4706f063ccaa578bd972
Mauricio Mutte <mauriciomutte.mm@gmail.com>
2023-08-26 16:08:21 -0300

    feat(auth): add auth service logics

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,51 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import * as argon from 'argon2';
 
-@Injectable({})
+import { PrismaService } from '../prisma/prisma.service';
+
+import { CreateUserDto, LoginDto } from './dto';
+
+@Injectable()
 export class AuthService {
-  async signUp() {
-    return { message: 'You are signing UP! =D' };
+  constructor(private prisma: PrismaService) {}
+
+  async signUp(createUserDto: CreateUserDto) {
+    const hash = await argon.hash(createUserDto.password);
+
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: createUserDto.email,
+          hash,
+        },
+      });
+
+      return { id: user.id, email: user.email };
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Email already exists');
+        }
+      }
+
+      throw error;
+    }
   }
 
-  async signIn() {
-    return { message: 'You are signing IN! =D' };
+  async signIn(loginDto: LoginDto) {
+    const user = await this.prisma.user.findUnique({
+      where: { email: loginDto.email },
+    });
+    if (!user) {
+      throw new ForbiddenException('Wrong email or password');
+    }
+
+    const isPasswordValid = await argon.verify(user.hash, loginDto.password);
+    if (!isPasswordValid) {
+      throw new ForbiddenException('Wrong email or password');
+    }
+
+    return { id: user.id, email: user.email };
   }
 }
