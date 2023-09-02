e97b87ccf2e2b56420486933077ae0a3f3916e1a
Mauricio Mutte <mauriciomutte.mm@gmail.com>
2023-09-02 20:31:30 -0300

    feat(user): add service edit user

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common';
 
 import { PrismaService } from '../prisma/prisma.service';
 
+import { EditUserDto } from './dto';
+
 @Injectable()
 export class UserService {
   constructor(private prisma: PrismaService) {}
@@ -9,6 +11,20 @@ export class UserService {
   async getMe(id: string) {
     const user = await this.prisma.user.findUnique({
       where: { id },
+    });
+
+    return {
+      id: user?.id,
+      email: user?.email,
+      firstName: user?.firstName,
+      lastName: user?.lastName,
+    };
+  }
+
+  async editUser(id: string, dto: EditUserDto) {
+    const user = await this.prisma.user.update({
+      where: { id },
+      data: { ...dto },
     });
 
     return {
